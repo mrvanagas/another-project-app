1a4681391a2a68644b28fb03bae1ad02a5957713
mrvanagas <lukonelis1@gmail.com>
2021-09-30 15:52:23 +0300

    feat: readme update

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Header } from 'semantic-ui-react'
 
 const Home = () => {
     return (
         <div data-testid="home-1">
-            Home
+            <Header as='h1'>Home page</Header>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, a. Aliquid dolorum facilis at similique natus neque earum nihil blanditiis a non excepturi atque sapiente eligendi, unde rerum odit molestias!</p>
         </div>
     )
 }
